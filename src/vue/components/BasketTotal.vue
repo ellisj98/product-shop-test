61935971776b3882a544c7952cb82441ed6dab32
Ellis Johnston <86981893+ellismonva@users.noreply.github.com>
2023-03-14 20:07:34 +0000

    Change usage of shopStore in BasketTotal.

--- src/vue/components/BasketTotal.vue
+++ src/vue/components/BasketTotal.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import useShopStore from '../stores/shop';
 import AppHeader from "./AppHeader.vue";
-const basketTotal = useShopStore().getBasketTotalItems;
+const shopStore = useShopStore();
 </script>
 
 <template>
    <span class="u-block u-ratio-1-1 u-w-10 u-rounded-full u-bg-brand-primary u-text-white u-relative">
-        <span class="u-center">{{ basketTotal }}</span>
+        <span class="u-center">{{ shopStore.getBasketTotalItems }}</span>
    </span>
 </template>
