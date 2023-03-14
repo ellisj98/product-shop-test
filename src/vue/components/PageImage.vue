762e691c4faffbbf697256bed970745d871878ec
Ellis Johnston <86981893+ellismonva@users.noreply.github.com>
2023-03-14 18:18:22 +0000

    Change page header size.

--- src/vue/components/PageImage.vue
+++ src/vue/components/PageImage.vue
@@ -4,5 +4,7 @@ const image = asset('bg.jpg');
 </script>
 
 <template>
-    <img :src="image" alt="Page graphic image" class="u-w-full" />
+    <div class="u-ratio-page-header u-relative">
+        <img :src="image" alt="Page graphic image" class="u-cover" />
+    </div>
 </template>
