3d2f44b4f614043e66534791a79621abea5a5098
Ellis Johnston <86981893+ellismonva@users.noreply.github.com>
2023-03-14 21:43:05 +0000

    Change border on logo.

--- src/vue/components/AppHeader.vue
+++ src/vue/components/AppHeader.vue
@@ -3,7 +3,7 @@
 <template>
     <header class="u-p-5">
         <div class="o-wrapper u-flex u-items-center u-justify-between">
-            <div class="u-uppercase u-border u-border-brand-primary u-p-1.5 u-px-2 u-text-sm u-bg-white">
+            <div class="u-uppercase u-border-2 u-border-brand-primary u-p-1.5 u-px-2 u-text-sm u-bg-white">
                 VF-Apparel<sup class="u-uppercase">co</sup>
             </div>
             <slot></slot>
